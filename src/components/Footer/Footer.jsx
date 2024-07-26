d8cdcd297d4d7da0be04dc0aaacbadbe9bfdc0f2
MavlonTurgunof <mavlondev2023@gmail.com>
2024-07-26 07:49:54 +0500

    Footer is implemented and landing page made fully responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,10 +14,33 @@ function Footer() {
             voluptatem culpa similique cum ipsam repudiandae perspiciatis,
             perferendis doloremque in omnis, inventore qui?
           </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
         </div>
-        <div className="footer-content-center"></div>
-        <div className="footer-content-right"></div>
+        <div className="footer-content-center">
+          <h2>Company</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>Get in touch</h2>
+          <ul>
+            <li>+998-95-042-03-19</li>
+            <li>mavlondev2023@gmail.com</li>
+          </ul>
+        </div>
       </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 Tomato.com - All Right Reserved.
+      </p>
     </div>
   );
 }
